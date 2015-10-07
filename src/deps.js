03316b0c50dd5a855063ae919f252af982f1125e
Javier Aragón <javier.aragon@geographica.gs>
2015-10-07 13:04:58 +0200

    template inicial

---
+++ src/deps.js
@@ -0,0 +1,13 @@
+var deps = {};
+
+deps.templateFolder = 'js/template';
+
+deps.JS = [];
+
+
+
+deps.lessFile = 'css/styles.less';
+
+if (typeof exports !== 'undefined') {
+    exports.deps = deps;
+}
